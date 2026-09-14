b09004632aecbdf2cd6a1ca02dbf190a8835ef1a
01ls1z28-coder <01ls1z28@gmail.com>
2026-09-14 14:03:21 -0500

    Garage admin: update js/garage-data.js

--- js/garage-data.js
+++ js/garage-data.js
@@ -3075,7 +3075,7 @@ window.GARAGE_DATA = [
     "Transmission": "Manual"
   },
   {
-    "Name": "2011 Kawasaki Ninja ZX-10R (Enriques)",
+    "Name": "2011 Kawasaki Ninja ZX-10R (Enrique's)",
     "Horsepower": 197,
     "WeightLbs": 427,
     "DragCoefficient": 0.58,
@@ -5075,7 +5075,7 @@ window.GARAGE_DATA = [
     "Transmission": "Manual"
   },
   {
-    "Name": "2011 - 2014 Ford F-150 Enrique's 3.5EB",
+    "Name": "2011 Ford F-150 3.5 EcoBoost (Enrique's)",
     "Horsepower": 462,
     "WeightLbs": 5688,
     "DragCoefficient": 0.44,
